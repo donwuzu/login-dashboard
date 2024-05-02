47e0ef080775dda7bc20d32c69abf1920d82af76
donwuzu <davmurage@gmail.com>
2024-05-02 13:44:32 +0300

    bd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
 import './App.css';
 
 function App() {
   return (
-    <div >
-     Welcome, Sir.
+    <div className="app">
+      <header>
+        <h1>Welcome Sir</h1>
+      </header>
+      <main>
+        <p>This is a simple React application.</p>
+      </main>
+      <footer>
+        <p>Footer content</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
